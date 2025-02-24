9b91ab345fe3b6cc93cac2d85efc48e04b5a0b35
AnastasiaCherkashyna <anastasia.cherkashyna.ukr@gmail.com>
2025-02-24 17:03:29 +0100

    isItemAddedToCartByText added

--- src/test/java/com/ait/homeworks/TestBase.java
+++ src/test/java/com/ait/homeworks/TestBase.java
@@ -1,12 +1,16 @@
-package tests;
+package com.ait.homeworks;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class TestBase {
     WebDriver driver;
@@ -29,6 +33,11 @@ public class TestBase {
         return true;
     }
 
+    public boolean isTextPresens(By locator){
+        driver.findElement(locator);
+        return true;
+    }
+
     public void type(By locator, String text) {
         click(locator);
         driver.findElement(locator).clear();
@@ -37,5 +46,15 @@ public class TestBase {
 
     public void click(By locator) {
         driver.findElement(locator).click();
+    }
+
+    public boolean isItemAddedToCartByText(String text){
+        List<WebElement> items = driver.findElements(By.cssSelector(".product-name"));
+
+        for (WebElement element : items){
+            if(element.getText().contains(text))
+                return true;
+            }
+        return false;
     }
 }
