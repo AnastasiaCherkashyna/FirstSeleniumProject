d273c2abcd6c2c2c0c2c0e6d11ea6507c912feb7
AnastasiaCherkashyna <anastasia.cherkashyna.ukr@gmail.com>
2025-03-02 19:55:34 +0100

    Logger and Crossbrowser testing added

--- src/main/java/com/webshop/framework/BaseHelper.java
+++ src/main/java/com/webshop/framework/BaseHelper.java
@@ -1,7 +1,12 @@
 package com.webshop.framework;
 
+import com.google.common.io.Files;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.io.File;
 
 public class BaseHelper {
     WebDriver driver;
@@ -23,7 +28,28 @@ public class BaseHelper {
         }
     }
 
+    public void pause(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public void click(By locator) {
         driver.findElement(locator).click();
     }
-}
+
+    public String takeScreenshot(){
+        File tmp = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File screenshot = new File("screenshots/screen-" + System.currentTimeMillis() + ".png");
+
+        try {
+            Files.copy(tmp,screenshot);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return screenshot.getAbsolutePath();
+    }
+    }
+
+
