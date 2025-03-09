4fc8c280c2c7f1380038174a959f93b546ac09d4
AnastasiaCherkashyna <anastasia.cherkashyna.ukr@gmail.com>
2025-03-09 18:20:43 +0100

    DataProvider was added in CreateAccountTests

--- src/test/java/com/ait/demowebshop/test/CreateAccountTests.java
+++ src/test/java/com/ait/demowebshop/test/CreateAccountTests.java
@@ -2,20 +2,59 @@ package com.ait.demowebshop.test;
 
 import com.webshop.models.NewUser;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class CreateAccountTests extends TestBase {
 
-    @Test
+   /* @Test
     public void newUserRegistration(){
         app.getUser().clickOnRegistrationLink();
-        app.getUser().fillRegistrationForm(new NewUser().setFirstName("Milok")
+        app.getUser().fillRegistrationForm(new NewUser().setFirstName("Milok13")
                 .setLastName("Tong")
-                .setEmail("Milok@gm.com")
-                .setPassword("1234567")
-                .setConfirmPassword("1234567"));
+                .setEmail(UserData.EMAIL)
+                .setPassword(UserData.PASSWORD)
+                .setConfirmPassword(UserData.PASSWORD));
         app.getUser().ckickOnRegistrationButton();
+        app.getUser().pause(1000);
         Assert.assertTrue(app.getUser().isLogOutLinkPresent());
+    } */
+
+    @DataProvider
+    public Iterator<Object[]> addNewRegistration(){
+        List<Object[]> list = new ArrayList<>();
+        list.add(new Object[]{"Sara","Koch","sara@gm.com","1234567","1234567"});
+        list.add(new Object[]{"Sara1","Kochhhhhhhhhhhhhhhhhhh","sara1@gm.com","1234567","1234567"});
+        list.add(new Object[]{"Sara2","Koch","sara2@gm.com","123456789012345","123456789012345"});
+        list.add(new Object[]{"Sara3","Koch","sara3@gm.com","1234567890","1234567890"});
+
+
+        return list.iterator();
+    }
+
+
+    @Test(dataProvider = "addNewRegistration")
+    public void newUserRegistrationFromDataProvider(String name, String lastName, String email,
+                                                    String password, String repeatPassword){
+        app.getUser().clickOnRegistrationLink();
+        app.getUser().fillRegistrationForm(new NewUser().setFirstName(name)
+                .setLastName(lastName)
+                .setEmail(email)
+                .setPassword(password)
+                .setConfirmPassword(repeatPassword));
+        app.getUser().ckickOnRegistrationButton();
+        app.getUser().pause(1000);
+        Assert.assertTrue(app.getUser().isLogOutLinkPresent());
+    }
+
+    @AfterMethod
+    public void postCondition(){
+        app.getUser().clickOnLogOutLink();
     }
 
 }
